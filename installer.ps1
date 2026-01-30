5ae4142bd6e985eb9f62ad7ba481a2672921da89
Mateo Villanueva <167149318+mvillanueva988@users.noreply.github.com>
2026-01-30 00:55:11 -0300

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,12 +1,20 @@
 # Bootstrap ServiceKit
 $repo = "mvillanueva988/ServiceKit-Public"
-$url = "https://github.com/$repo/releases/latest/download/ServiceKit_v1.7.exe"
-$out = "$env:TEMP\ServiceKit_v1.7.exe"
+$file = "ServiceKit_v1.7.exe" # Asegúrate que coincida con tu versión actual
+$url = "https://github.com/$repo/releases/latest/download/$file"
+$out = "$env:TEMP\$file"
 
 Write-Host "Descargando ServiceKit desde GitHub..." -F Cyan
+
+# --- ESTA ES LA LINEA MAGICA ---
+# Desactiva la barra de progreso para liberar la velocidad de descarga
+$ProgressPreference = 'SilentlyContinue'
+# -------------------------------
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Ahora la descarga volará
 Invoke-WebRequest -Uri $url -OutFile $out
 
 Write-Host "Iniciando..." -F Green
-
 Start-Process $out
-
